6766628113f0cac8563de42b41655495c9ff5943
bexi <trreitmaier@msn.com>
2018-10-05 22:24:15 +0200

    created server setup with users, not tested since the client is not created

--- server.erl
+++ server.erl
@@ -1,6 +1,24 @@
 -module(server).
 -export([start/1,stop/1]).
 
+-record(server_state, {
+      name,
+      users
+}).
+
+%%
+init_state(ServerName) ->
+  #server_state{name = ServerName, users = [] } .
+
+%%
+%   - takes 2 params : state, request message
+%   - returns a tuple: new state, response message
+% need to have a function for each request type (connect, join, leave ...)
+req_handler(State, {connect, Pid, Nick}) ->
+  NewState = State#server_state{users = [Nick | State#server_state.users]},
+  io:format("The users: ~p~n", [State#server_state.users]),
+  {reply, ok, NewState} .
+
 % Start a new server process with the given name
 % Do not change the signature of this function.
 start(ServerAtom) ->
@@ -8,11 +26,12 @@ start(ServerAtom) ->
     % - Spawn a new process which waits for a message, handles it, then loops infinitely
     % - Register this process to ServerAtom
     % - Return the process ID
-    not_implemented.
+    genserver:start(ServerAtom, init_state(ServerAtom), fun req_handler/2) .
 
 % Stop the server process registered to the given name,
 % together with any other associated processes
 stop(ServerAtom) ->
     % TODO Implement function
+    genserver:stop(ServerAtom),
     % Return ok
-    not_implemented.
+    ok .
