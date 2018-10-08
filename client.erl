879e7f4bcd3de5d1d17fcf42efb9d299d970ed29
bexi <trreitmaier@msn.com>
2018-10-08 13:34:54 +0200

    join command now works

--- client.erl
+++ client.erl
@@ -6,7 +6,8 @@
 -record(client_st, {
     gui, % atom of the GUI process
     nick, % nick/username of the client
-    server % atom of the chat server
+    server, % atom of the chat server
+    channels % existing channels on the server
 }).
 
 % Return an initial state record. This is called from GUI.
@@ -15,22 +16,53 @@ initial_state(Nick, GUIAtom, ServerAtom) ->
     #client_st{
         gui = GUIAtom,
         nick = Nick,
-        server = ServerAtom
+        server = ServerAtom,
+        channels = []
     }.
 
-% handle/2 handles each kind of request from GUI
-% Parameters:
-%   - the current state of the client (St)
-%   - request data from GUI
-% Must return a tuple {reply, Data, NewState}, where:
-%   - Data is what is sent to GUI, either the atom `ok` or a tuple {error, Atom, "Error message"}
-%   - NewState is the updated state of the client
+addChannel(State, Channel) ->
+  NewState = State#client_st{channels = [ Channel | State#client_st.channels ]} .
+
+% TODO fix
+leaveChannel(State, Channel) ->
+  Update = lists:delete(Channel, State#client_st.channels ),
+  NewState = State#client_st{channels = Update} .
+
+
+isUserJoined(State, Channel) ->
+  lists:member(Channel, State#client_st.channels).
 
 % Join channel
-handle(St, {join, Channel}) ->
-    % TODO: Implement this function
-    % {reply, ok, St} ;
-    {reply, {error, not_implemented, "join not implemented"}, St} ;
+% TODO REDO
+handle(State, {join, Channel}) ->
+
+  case isUserJoined(State, list_to_atom(Channel)) of
+    % user is not in the channel
+    false ->
+    	case whereis(list_to_atom(Channel)) of
+        % need to create channel
+    	  undefined ->
+    		case catch genserver:request(State#client_st.server, {createchannel, self(), Channel}) of
+    		    ok -> % Request ok, createchannel operation succeeded. Then join channel.
+      			  genserver:request(list_to_atom(Channel), {join, self()}),
+      			  io:format("Now the client has joined an newly created channel. ~n"),
+      			  % Then, update the clients channel list (insert new channel).
+              NewState = addChannel(State, list_to_atom(Channel)),
+      			  {reply, ok, NewState};
+    		    {'EXIT', Reason} ->
+    			    {reply, {error, server_not_reached, "Something went wrong when trying to join, probably no connection to server.~n"}, State}
+    	  end;
+	    _ ->
+		  genserver:request(list_to_atom(Channel), {join, self()}),
+		  io:format("Now the client has joined an already existing channel. ~n"),
+      NewState = addChannel(State, list_to_atom(Channel)),
+		  {reply, ok, NewState}
+    end;
+    true ->
+	io:format("The channel has already been joined. ~n"),
+	{reply, {error, user_already_joined, "Already joined~n"}, State}
+end;
+
 
 % Leave channel
 handle(St, {leave, Channel}) ->
--- server.erl
+++ server.erl
@@ -3,35 +3,57 @@
 
 -record(server_state, {
       name,
+      users,
+      channels
+}).
+
+-record(channel_state, {
+      name,
       users
 }).
 
-%%
-init_state(ServerName) ->
-  #server_state{name = ServerName, users = [] } .
-
-%%
-%   - takes 2 params : state, request message
-%   - returns a tuple: new state, response message
-% need to have a function for each request type (connect, join, leave ...)
-req_handler(State, {connect, Pid, Nick}) ->
-  NewState = State#server_state{users = [Nick | State#server_state.users]},
-  io:format("The users: ~p~n", [State#server_state.users]),
-  {reply, ok, NewState} .
+% set intial state for the server
+initState(ServerName) ->
+  #server_state{name = ServerName, users = [], channels = [] } .
 
 % Start a new server process with the given name
-% Do not change the signature of this function.
 start(ServerAtom) ->
-    % TODO Implement function
     % - Spawn a new process which waits for a message, handles it, then loops infinitely
     % - Register this process to ServerAtom
     % - Return the process ID
-    genserver:start(ServerAtom, init_state(ServerAtom), fun req_handler/2) .
+    genserver:start(ServerAtom, initState(ServerAtom), fun serverHandle/2) .
 
 % Stop the server process registered to the given name,
 % together with any other associated processes
 stop(ServerAtom) ->
-    % TODO Implement function
-    genserver:stop(ServerAtom),
-    % Return ok
-    ok .
+    genserver:stop(ServerAtom).
+
+%% Server handle functions -----------------------------------------------------
+
+serverHandle(State, {createchannel, Pid, Channel}) ->
+	% Just create new channel.
+  genserver:start(list_to_atom(Channel), initChannel(list_to_atom(Channel)), fun channelhandle/2),
+	NewState = State#server_state{channels = [ list_to_atom(Channel) | State#server_state.channels] }, %State#server_state.channels ++ [list_to_atom(Channel)]
+	io:format("The [servers] list of channels: ~p~n", [State#server_state.channels]),
+	{reply, ok, NewState};
+
+%%
+%   - takes 2 params : state, request message
+%   - returns a tuple: new state, response message
+% need to have a function for each request type (join, leave ...)
+serverHandle(State, {join, UserPid}) ->
+  NewState = State#channel_state{users = [UserPid | State#channel_state.users] },
+  {reply, ok, NewState}.
+
+%% Channel functions -----------------------------------------------------------
+initChannel(ChannelName) ->
+    #channel_state{
+          name = ChannelName,
+  				users = []
+  }.
+
+%% [Joining channel]
+% Add client to the list of clients who have joined the channel.
+channelhandle(State, {join, Pid}) ->
+      UpdatedState = State#channel_state{users = State#channel_state.users ++ [Pid] },
+      {reply, ok, UpdatedState}.
